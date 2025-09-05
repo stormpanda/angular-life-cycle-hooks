0805a21c70d6d6ebc8bd6221bd82c2a64e4060f5
Maik Thomalla <maikthomalla@googlemail.com>
2025-09-05 11:14:44 +0200

    feat: add child lifecycle component

---
+++ src/app/lifecycle-demo/child/child.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, OnDestroy, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  standalone: true,
+  imports: [],
+  templateUrl: './child.component.html',
+  styleUrl: './child.component.scss'
+})
+export class LifecycleChildComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('LifecycleChildComponent ngOnChanges', changes);
+  }
+
+  ngOnInit(): void {
+    console.log('LifecycleChildComponent ngOnInit');
+  }
+
+  ngDoCheck(): void {
+    console.log('LifecycleChildComponent ngDoCheck');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('LifecycleChildComponent ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('LifecycleChildComponent ngAfterContentChecked');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('LifecycleChildComponent ngAfterViewInit');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('LifecycleChildComponent ngAfterViewChecked');
+  }
+
+  ngOnDestroy(): void {
+    console.log('LifecycleChildComponent ngOnDestroy');
+  }
+}
